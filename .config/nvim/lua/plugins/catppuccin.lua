840b0b655b43b0edfce5f19e848168322ac60cfd
luminara <me@kiranchhetri.com.np>
2025-12-24 15:50:58 +0545

    Add auto theme switching to neovim

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -6,9 +6,13 @@ return {
     priority = 1000,
     config = function()
       require("catppuccin").setup({
+        background = {
+          light = "latte",
+          dark = "mocha",
+        },
         transparent_background = true,
       })
-      vim.cmd.colorscheme("catppuccin-mocha")
+      vim.cmd.colorscheme("catppuccin")
     end,
   },
 }
